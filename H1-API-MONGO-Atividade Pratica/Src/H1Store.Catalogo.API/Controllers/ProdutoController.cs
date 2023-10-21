6ad7e643c2aa3da337c0e329c5834885819605e8
MathboyL3 <danielcurijogogames@yahoo.com>
2023-10-21 13:29:35 -0300

    Atividade Pratica Adicionada

---
+++ H1-API-MONGO-Atividade Pratica/Src/H1Store.Catalogo.API/Controllers/ProdutoController.cs
@@ -0,0 +1,95 @@
+﻿using H1Store.Catalogo.Application.Interfaces;
+using H1Store.Catalogo.Application.ViewModels;
+using H1Store.Catalogo.Data.Providers.MongoDb.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Xml.Linq;
+
+namespace H1Store.Catalogo.API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ProdutoController : ControllerBase
+	{
+		private readonly IProdutoService _produtoService;
+		public ProdutoController(IProdutoService produtoService)
+		{
+			_produtoService = produtoService;
+		}
+
+		[HttpPost]
+		[Route("Adicionar")]
+		public async Task<IActionResult> Adicionar(NovoProdutoViewModel novoProdutoViewModel)
+		{
+			await _produtoService.Adicionar(novoProdutoViewModel);
+
+			return Ok();
+		}
+
+		[HttpPut]
+		[Route("AtualizarEstoque/{id}/{quantidade}")]
+		public async Task<IActionResult> AtualizarEstoque(Guid id, int quantidade)
+		{
+			await _produtoService.AtualizarEstoque(id, quantidade);
+
+			return Ok($"Estoque modificado em {quantidade} com sucesso!");
+		}
+
+		[HttpPut]
+		[Route("Atualizar/{id}")]
+
+		public async Task<IActionResult> Atualizar(Guid id, [FromBody]NovoProdutoViewModel produto)
+		{
+			await _produtoService.Atualizar(id, produto);
+
+			return Ok("Produto modificado com sucesso!");
+		}
+
+		[HttpPut]
+		[Route("Desativar/{id}")]
+		public async Task<IActionResult> Desativar(Guid id)
+		{
+			await _produtoService.Desativar(id);
+
+			return Ok("Produto desativado com sucesso");
+		}
+
+		[HttpPut]
+		[Route("Ativar/{id}")]
+		public async Task<IActionResult> Ativar(Guid id)
+		{
+			await _produtoService.Ativar(id);
+
+			return Ok("Produto ativado com sucesso");
+		}
+
+		[HttpGet]
+		[Route("ObterPorNome/{nome}")]
+		public async Task<IActionResult> ObterPorNome(string nome)
+		{
+			return Ok(await _produtoService.ObterPorNome(nome));
+		}
+
+		[HttpGet]
+		[Route("ObterPorId/{id}")]
+		public async Task<IActionResult> ObterPorId(Guid id)
+		{
+			return Ok(await _produtoService.ObterPorId(id));
+		}
+
+		[HttpPut]
+		[Route("AlterarPreco/{id}/{preco}")]
+		public async Task<IActionResult> AlterarPreco(Guid id, decimal preco)
+		{
+			await _produtoService.AlterarPreco(id, preco);
+			return Ok("Preço alterado com sucesso!");
+		}
+
+		[HttpGet]
+		[Route("ObterTodos")]
+		public IActionResult GetAll()
+		{
+			return Ok(_produtoService.ObterTodos());
+		}
+	}
+}
---
+++ H1-API-MONGO-Atividade Pratica/Src/H1Store.Catalogo.Application/Services/ProdutoService.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using H1Store.Catalogo.Application.Interfaces;
+using H1Store.Catalogo.Application.ViewModels;
+using H1Store.Catalogo.Domain.Entities;
+using H1Store.Catalogo.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1Store.Catalogo.Application.Services
+{
+	public class ProdutoService : IProdutoService
+	{
+		#region - Construtores
+		private readonly IProdutoRepository _produtoRepository;
+		private IMapper _mapper;
+
+		public ProdutoService(IProdutoRepository produtoRepository, IMapper mapper)
+		{
+			_produtoRepository = produtoRepository;
+			_mapper = mapper;
+		}
+		#endregion
+
+		#region - Funções
+		public async Task Adicionar(NovoProdutoViewModel novoProdutoViewModel)
+		{
+			var novoProduto = _mapper.Map<Produto>(novoProdutoViewModel);
+			await _produtoRepository.Adicionar(novoProduto);
+
+		}
+
+		public async Task Desativar(Guid id)
+		{
+			var buscaProduto = await _produtoRepository.ObterPorId(id);
+
+			if(buscaProduto == null)  throw new ApplicationException("Não é possível desativar um produto inexistente!");
+
+			buscaProduto.Desativar();
+
+			await _produtoRepository.Desativar(buscaProduto);
+
+		}
+
+		public async Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int codigo)
+		{
+			return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterPorCategoria(codigo));
+
+		}
+
+		public async Task<ProdutoViewModel> ObterPorId(Guid id)
+		{
+			return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterPorId(id));
+		}
+
+		public IEnumerable<ProdutoViewModel> ObterTodos()
+		{
+			return _mapper.Map<IEnumerable<ProdutoViewModel>>(_produtoRepository.ObterTodos());
+		}
+		#endregion
+
+		public async Task Atualizar(Guid id, NovoProdutoViewModel produto)
+		{
+			var prod = _mapper.Map<Produto>(produto);
+			prod.CodigoId = id;
+			await _produtoRepository.Atualizar(prod);
+		}
+
+		public async Task<ProdutoViewModel> ObterPorNome(string nome)
+		{
+			return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterPorNome(nome));
+		}
+
+		public async Task AlterarPreco(Guid id, decimal newPreco)
+		{
+			var produto = await _produtoRepository.ObterPorId(id);
+			
+			if(produto == null) throw new ArgumentNullException("Produto não existente!");
+			if(newPreco <= 0) throw new ArgumentNullException("O preço não não pode ser <= 0");
+
+			await _produtoRepository.AlterarPreco(produto, newPreco);
+		}
+		public async Task AtualizarEstoque(Guid id, int quantidade)
+		{
+			var buscaProduto = await _produtoRepository.ObterPorId(id);
+			if(quantidade > 0)
+				buscaProduto.ReporEstoque(quantidade);
+			else
+				buscaProduto.DebitarEstoque(quantidade * -1);
+
+			await _produtoRepository.AtualizarEstoque(buscaProduto);
+
+		}
+
+		public async Task Ativar(Guid id)
+		{
+			var produto = await _produtoRepository.ObterPorId(id);
+			produto.Ativar();
+			await _produtoRepository.Ativar(produto);
+		}
+	}
+}
